c118bcb829684365ac506d1ae174c6f7af70cbc4
Malte <91071750+Loxiuz@users.noreply.github.com>
2023-09-22 14:21:08 +0200

    Update backend.js

--- backend/backend.js
+++ backend/backend.js
@@ -1,14 +1,19 @@
 "use strict";
 
-// 
+//
 //  Backend Node.js app for musicoolio website.
 //  Talks to azure database, and does cool stuff!
-// 
+//
 
 // Import Express, FS and CORS.
 import express from "express";
 import cors from "cors";
 import connection from "./database.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
 
 // Default function for printing error or returning results.
 function errorResult(err, result, response) {
@@ -18,16 +23,26 @@ function errorResult(err, result, response) {
     response.json(result);
   }
 }
-
-const app = express();
-
-app.use(express.json());
-app.use(cors());
-
+/* ---------- Routes for Artists ---------- */
 // Get request for artists (all, ordered by name)
 app.get("/artists", async (request, response) => {
   connection.query("SELECT * FROM artists ORDER BY name;", (err, result) => {
     // print error or respond with result.
     errorResult(err, result, response);
-    });
   });
+});
+/* ---------- Routes for Albums ---------- */
+app.get("/artists", async (request, response) => {
+  connection.query("SELECT * FROM albums ORDER BY name;", (err, result) => {
+    // print error or respond with result.
+    errorResult(err, result, response);
+  });
+});
+
+/* ---------- Routes for Tracks ---------- */
+app.get("/artists", async (request, response) => {
+  connection.query("SELECT * FROM tracks ORDER BY title;", (err, result) => {
+    // print error or respond with result.
+    errorResult(err, result, response);
+  });
+});
