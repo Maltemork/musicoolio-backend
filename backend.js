f2ed6c5bc6ebe82b0248870d15f26f5e131141ec
Malte <91071750+Loxiuz@users.noreply.github.com>
2023-10-22 21:14:04 +0200

    Update backend.js

--- backend.js
+++ backend.js
@@ -202,14 +202,8 @@ app.delete("/tracks/:id", async (request, response) => {
 app.post("/tracks", async (request, response) => {
   const reqBody = request.body;
   connection.query(
-    "INSERT INTO tracks(title, releaseDate, duration, trackNo, artistName) VALUES(?, ?, ?, ?, ?)",
-    [
-      reqBody.title,
-      reqBody.releaseDate,
-      reqBody.duration,
-      reqBody.trackNo,
-      reqBody.artistName,
-    ],
+    "INSERT INTO tracks(title, duration, trackNo, artistName) VALUES(?, ?, ?, ?)",
+    [reqBody.title, reqBody.duration, reqBody.trackNo, reqBody.artistName],
     (err, result) => {
       // print error or respond with result.
       errorResult(err, result, response);
